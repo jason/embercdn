b1f3b83c9e12a7f143dccf4b014ce776939f9357
Jason Wong <technique@gmail.com>
2013-05-18 07:39:36 -0700

    initial commit with working routes

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a simple Ember experiment, to learn routing, variables and more, using the CDN signup at [http://www.i5labs.com/cdnIntroduction.html](http://www.i5labs.com/cdnIntroduction.html) as a base.
+
+TO DO
+  * When sign up is clicked, replace with specific plan fragment
+  * Ensure plan shows consistently (clicking how still shows the correct plan if plan is chosen)
+  * Integrate design from i5labs
+  * Integrate with [https://github.com/jason/closeioweb2lead](https://github.com/jason/closeioweb2lead)
+  * Deploy to Heroku
---
+++ js/app.js
@@ -0,0 +1,90 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+  this.route('how');
+  this.route('advantage');
+  this.resource('plans', function() {
+    this.resource('plan', { path: ':plan_id'});
+  });
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return App.Plan.find();
+  }
+});
+//App.PlansRoute = Ember.Route.extend({
+//   model: function() {
+//     return App.Plan.find();
+//   }  
+//});
+//App.ApplicationController = Ember.Controller.extend({
+//  currentSidebarContent: 'intro',
+
+//   changeSidebarContent: function() {
+//     this.render({ outlet: this.get('currentSidebarContent'); });
+
+//   }.property('currentSidebarContent')
+// });
+App.IndexRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render('intro', { outlet: 'intro' });
+   }
+ });
+App.HowRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render('how', { outlet: 'how' });
+  }
+});
+App.AdvantageRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render({ outlet: 'advantage' });
+  }
+});
+
+App.ApplicationController = Ember.ObjectController.extend({
+  click: function(evt) {
+    $(evt).toggle();
+  }
+});
+App.PlansController = Ember.ObjectController.extend({
+  // initial value
+  isExpanded: false,
+
+  expand: function() {
+    this.set('isExpanded', true);
+  },
+
+  contract: function() {
+    this.set('isExpanded', false);
+  }
+});
+App.Store = DS.Store.extend({
+    revision: 12,
+    adapter: 'DS.FixtureAdapter'
+});
+
+App.Plan = DS.Model.extend({
+  // e.g. Surface, Sectional, Pathways, Visual Glossary
+  transfer: DS.attr('number'),
+  storage: DS.attr('number'),
+  price: DS.attr('number')
+});
+
+App.Plan.FIXTURES = [{
+  id: 1,
+  transfer: 50,
+  storage: 1,
+  price: 100
+}, {
+  id: 2,
+  transfer: 150,
+  storage: 10,
+  price: 250
+}, {
+  id: 3,
+  transfer: 500,
+  storage: 25,
+  price: 500
+}];
